fbd0c4b4cdf41e004b957e9bd69f5117824b52b3
Aarsh Patel <aarshpatel2004@gmail.com>
2024-02-22 13:08:01 -0500

    Complete Create Degree Constructor Stub #44, Complete Create getProgram Method Stub #45

--- DegreeClass/Degree.java
+++ DegreeClass/Degree.java
@@ -1,9 +1,16 @@
 package DegreeClass;
 
+import java.util.ArrayList;
 /**
  * 
  * @author Aarsh Patel
  */
 public class Degree {
+    public Degree(String degreeType, String subjectName) {
+        
+    }
     
+    public ArrayList<Program> getPrograms() {
+        return new ArrayList<Program>();
+    }
 }
