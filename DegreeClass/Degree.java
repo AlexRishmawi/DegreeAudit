5d39211441f3f0d7276bb2ead161082d63b14580
Aarsh Patel <aarshpatel2004@gmail.com>
2024-02-23 13:37:55 -0500

    Done create #89, 82, #1, #44, #45, #82, #104, #106, #108, #46, #51, #55, #73, #77, #78, #84

--- DegreeClass/Degree.java
+++ DegreeClass/Degree.java
@@ -6,11 +6,65 @@ import java.util.ArrayList;
  * @author Aarsh Patel
  */
 public class Degree {
-    public Degree(String degreeType, String subjectName) {
+    // Program variables
+    private ArrayList<Program> programs;
+    private int totalCreditRequired;
+    private ArrayList<Course> totalCoursesRequired;
+    private String subjectName;
+    private String degreeType;
+
+    // Course Variables
+    private ArrayList<Course> completeCourses;
+    private int completeCredit;
+
+    // Course Generator
+    private Semester currentSemester;
+    private ArrayList<Semester> allSemesters;
+
+    public Degree(String degreeType, String subjectName, ArrayList<Program> programs, 
+    int totalCreditRequired, ArrayList<Course> totalCoursesRequired, ArrayList<Course> completeCourses, 
+    int completeCredit, Semester currentSemester, ArrayList<Course> recomendedSemCourses, 
+    ArrayList<Semester> allSemesters) {
         
     }
     
     public ArrayList<Program> getPrograms() {
         return new ArrayList<Program>();
+    }
+
+    public int getTotalCreditRequired() {
+        return 0;
+    }
+
+    public ArrayList<Course> getTotalCoursesRequired() {
+        return new ArrayList<Course>();
+    }
+
+    public boolean addProgram(Program program) {
+        return false;
+    }
+
+    public boolean removeProgram(UUID programID) {
+        return false;
+    }
+
+    public boolean setCourseCompleted(Course course, String grade) {
+        return false;
+    }
+
+    public boolean checkPrerequisites(Course course) {
+        return false;
+    }
+    
+    public boolean equals(Degree degree) {
+        return false;
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public String majorMapToString() {
+        return "";
     }
 }
