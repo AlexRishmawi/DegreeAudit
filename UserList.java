75a0dd1a614b28165654374cc5873bd8471a170a
pakshpatel1@gmail.com <pakshpatel1@gmail.com>
2024-02-24 13:10:26 -0500

    Finsihed #171, #172, #173, #174, #175, and added @author

--- UserList.java
+++ UserList.java
@@ -1,26 +1,43 @@
+/**
+ * @author Paksh Patel
+ */
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class UserList {
     private static UserList userList;
     private ArrayList<User> users;
 
-    private UserList() {
-    
+    private UserList(){
+        users = new ArrayList<User>();
     }
 
     public UserList getInstance(){
+        if(userList == null){
+            userList = new UserList();
+        }
+        return userList;
+    }
+
+    public User getUser(UUID id, String password){
+        for(User user : users){
+            if(user.getID().equals(id)){
+                return user;
+            }
+        }
         return null;
     }
 
-    public User getUser(String uscID, String password){
-        return null;
-    }
-
-    public boolean findUser(String uscID){
+    public boolean findUser(UUID id, String password){
+        for(User user : users){
+            if(user.getID().equals(id)){
+                return true;
+            }
+        }
         return false;
     }
 
     public ArrayList<User> getAllUsers(){
-        return null;
+        return users;
     }
 }
