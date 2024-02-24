7d1549f0b881b05df05ce36ccbf31c99496d837c
mesaAlex <alexmesa2004@gmail.com>
2024-02-24 01:49:40 -0500

    fixed #80, #49, #47, #123, #120, #11, #112, #116, #113, #115

--- UI.java
+++ UI.java
@@ -1,3 +1,18 @@
 public class UI {
+    private Scanner scanner;
+    private System application;
+    
+    public UI(Scanner scanner, System application) {
+        this.scanner = scanner;
+        this.application = application;
+    }
+    
+    public void run() {
 
+    }
+    
+    private void display() {
+        
+    }
 }
+
