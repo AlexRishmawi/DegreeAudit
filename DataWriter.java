a323a1e2c59127f1b0fb4a1ce07587db82528bca
mesaAlex <alexmesa2004@gmail.com>
2024-02-24 14:01:28 -0500

    fixed #177, # 205, #206, #207

--- DataWriter.java
+++ DataWriter.java
@@ -1,14 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class DataWriter {
 
     public void writeUser(User user, char mode) {
-
+        writeFile(DataConstants.STUDENT_FILE_NAME, user.toString(), mode == 'a');
     }
 
     public void writeProgram(Program program, char mode) {
-
+        writeFile(DataConstants.PROGRAM_FILE_NAME, program.toString(), mode == 'a');
     }
 
-    public void writeCourse(String file, Course course) {
+    public void writeCourse(String file, Course course, char mode) {
+        writeFile(file, course.toString(), mode == 'a');
+    }
 
+    private void writeFile(String filePath, String content, boolean append) {
+        try (FileWriter fw = new FileWriter(filePath, append);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+            out.println(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
