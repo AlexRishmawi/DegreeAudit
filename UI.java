ddf1f5518aece47f395c3bae0dff00a73830b9cd
AlexRishmawi <Rishmaa@email.sc.edu>
2024-03-17 19:34:20 -0400

    Continued work on scenario 1

--- UI.java
+++ UI.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class UI {
     private Scanner scanner;
     private System application;
     
-    public UI(Scanner scanner, System application) {
+    public UI(Scanner scanner) { //System application) {
         this.scanner = scanner;
-        this.application = application;
+        //this.application = application;
     }
     
     public void run() {
         DegreeWork degreeWork = new DegreeWork();
+        Student student = degreeWork.createStudent("Brax", "West", "Bwest@email.sc.edu", "password", "Junior", null, null, degree, 0, 0, "Good Standing");
         degreeWork.login("Brax", "West", "password");
+        Degree degree = new Degree("Bachelor", "Computer Science", 120, null, null);
+        degreeWork.setCurrentUser(student);
         degreeWork.displayDegreeProgress();
 
+        
     }
     
     private void display() {
         
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        UI ui = new UI(scanner);
+        ui.run();
     }
 }
 
