23484fc1bc0bafdd40323ccdc78fcc437a172ce8
AlexRishmawi <Rishmaa@email.sc.edu>
2024-03-17 22:29:42 -0400

    Changed scenario 1

--- UI.java
+++ UI.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap;
 public class UI {
     private Scanner scanner;
     private System application;
@@ -11,8 +12,9 @@ public class UI {
     
     public void run() {
         DegreeWork degreeWork = new DegreeWork();
-        Degree degree = new Degree("Bachelor", "Computer Science", 120, null, null);
-        Student student = degreeWork.createStudent("Brax", "West", "Bwest@email.sc.edu", "password","X23456789", "Junior", null, null, degree, 0, 0, "Good Standing");
+        HashMap<Course, Integer> majorCourses = new HashMap<Course, Integer>();
+        Degree degree = new Degree("Bachelor", "Computer Science", 120, majorCourses, new ArrayList<ElectiveCategory>());
+        Student student = degreeWork.createStudent("Brax", "West", "Bwest@email.sc.edu", "password","X23456789", "Junior", new Advisor("John", "Doe", "jDoe@email.sc.edu", "password", false), new ArrayList<>(), degree, 0, 0, "Good Standing");
         degreeWork.login("Brax", "West", "password");
         
         degreeWork.setCurrentUser(student);
